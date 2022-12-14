0e0c0bf320aacba4b70dc3654c1937f7bf76863e
Zhong Zhenyu <nczzy1997@gmail.com>
2022-12-14 15:52:21 +0800

    Update: 2022/12/14 15:52:19

--- batchfile/ssh-tunnel.bat
+++ batchfile/ssh-tunnel.bat
@@ -1,9 +1,6 @@
-title SSH Tunnel
-
 ssh ^
   -L localhost:21122:10.10.2.211:22 ^
   -L localhost:21222:10.10.2.212:22 ^
   -L localhost:21522:10.10.2.215:22 ^
   -L localhost:21722:10.10.2.217:22 ^
-  -L localhost:22022:10.10.2.220:22 ^
   lidongwen@10.10.1.210
